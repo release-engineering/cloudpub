98cda896a1c2a2d8092e99d869e3f5b110ad81c4
Jordan Reidy <jreidy@redhat.com>
2024-02-13 08:14:31 -0500

    Release v0.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 0.5.1 - 2024-02-13
+
+- Azure: Include SV in SAS comparison exclusion
+- Remove JSON conversion debug logs
+
 ## 0.5.0 - 2024-01-XX
 
 - Azure: Check first for draft instead live
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='cloudpub',
     description='Services for publishing products in cloud environments',
-    version='0.5.0',
+    version='0.5.1',
     keywords='stratosphere cloudpub cloudpublish',
     author='Jonathan Gangi',
     author_email='jgangi@redhat.com',
